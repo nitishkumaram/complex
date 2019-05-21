68ff09d7be65313879b6857cc06de770455761c6
nitishkumaram <nitishkumar@outlook.com>
2019-05-21 23:57:40 +0530

    Initial Commit

---
+++ server/index.js
@@ -0,0 +1,10 @@
+const keys = require('./keys');
+
+// Express App Setup
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
